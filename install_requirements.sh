8aae4f5d9041f0fdfed60dac2ec7760a2b83d57a
TheDevilWillBeBee <73064198+TheDevilWillBeBee@users.noreply.github.com>
2024-07-03 16:20:56 +0200

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -2,6 +2,6 @@ pip install torch==2.1.1 torchvision==0.16.1 --index-url https://download.pytorc
 pip install kaolin -f https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.1.1_cu121.html
 pip install torch_geometric==2.5.3
 pip install moviepy
-pip install pyyam
+pip install pyyaml
 pip install gdown
 pip install wandb
